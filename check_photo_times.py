8f56595d2a0417169666eb650a87eaa84287aa11
Justin <justinpinkney@gmail.com>
2018-12-05 22:16:09 +0000

    initial commit

---
+++ check_photo_times.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from datetime import datetime
+import sys
+import glob
+import os
+
+def get_all_creation_times(directory):
+    times = []
+    files = glob.glob(directory + "/*.jpg")
+    for im_file in files:
+        created_time = get_creation_time(im_file)
+        times.append({"filename": os.path.basename(im_file),
+                        "taken_at": created_time,
+                        })
+    return times
+
+def get_creation_time(filename):
+    im = Image.open(filename)
+    exif = im._getexif()
+    created_field = exif[0x9003]
+    return datetime.strptime(created_field, "%Y:%m:%d %H:%M:%S")
---
+++ parse_data.py
@@ -0,0 +1,29 @@
+import json
+import glob
+
+import pandas as pd
+
+import check_photo_times as util
+
+labels = "data/raw/labels.json"
+trees = "data/raw/trees.csv"
+images = "data/raw/images"
+
+with open(labels) as label_file:
+    label_data = json.loads(label_file.read())
+
+label_list = []
+for item in label_data:
+    label_list.append({'filename': item['External ID'],
+                        'tree_id': item['Label']['tree_id'],
+                        })
+
+label_df = pd.DataFrame(label_list).set_index("filename")
+
+
+taken_data = util.get_all_creation_times(images)
+taken_df = pd.DataFrame(taken_data).set_index("filename")
+
+db = label_df.join(taken_df)
+print(db.groupby("tree_id")['taken_at'].nunique())
+print(db["tree_id"].value_counts())
